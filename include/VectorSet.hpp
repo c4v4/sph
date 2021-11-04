b03f063407f17de9a9a2a67dbcb4b6084a402883
cava <francescocava95@gmail.com>
2021-11-04 10:38:31 +0100

    VectorSet bugfix

--- include/VectorSet.hpp
+++ include/VectorSet.hpp
@@ -1,6 +1,7 @@
 #ifndef CAV_VECTORSET_HPP
 #define CAV_VECTORSET_HPP
 
+#include <cstddef>
 #include <type_traits>
 #include <unordered_set>
 #include <vector>
@@ -14,8 +15,8 @@ namespace cav {
         using const_viter = typename std::vector<T>::const_iterator;
 
         /**
-         * @brief Since the set does not contain T, but *T,
-         * this hacky struct is used to make the *T behave like
+         * @brief Since the set does not contain T, but T*,
+         * this hacky struct is used to make the T* behave like
          * a T would do (for what the set is concerned).
          *
          * Note: it's still a ptr, so it has all the problems of
@@ -28,7 +29,6 @@ namespace cav {
             T* data() const { return base_addr + idx; }
             bool operator==(const TPtrWrap& other) const { return other.base_addr[other.idx] == base_addr[idx]; }
 
-        private:
             T*& base_addr;
             size_t idx;
         };
@@ -38,32 +38,29 @@ namespace cav {
         using const_siter = typename Set::const_iterator;
 
     public:
-        VectorSet() { }
+        VectorSet() : vec_data(new T*) { }
 
         template <typename Iter>
         VectorSet(Iter beg, Iter end) {
             reserve(end - beg);
-            for (auto it = beg; it != end; ++it) {
-                emplace_back(*it);
-            }
+            for (auto it = beg; it != end; ++it) { emplace_back(*it); }
         }
 
-        VectorSet(const VectorSet& other) : vec(other.vec), set(other.set) { vec_data = vec.data(); }
-
-        VectorSet(VectorSet&& other) noexcept : vec(std::move(other.vec)), set(std::move(other.set)) { vec_data = vec.data(); }
+        VectorSet(const VectorSet& other) : vec(other.vec), vec_data(new T*), set(other.set) { *vec_data = vec.data(); }
+        VectorSet(VectorSet&& other) noexcept : vec(std::move(other.vec)), vec_data(std::exchange(other.vec_data, nullptr)), set(std::move(other.set)) { }
+        ~VectorSet() { delete vec_data; }
 
         VectorSet& operator=(const VectorSet& other) {
             vec = other.vec;
             set = other.set;
-            vec_data = vec.data();
+            *vec_data = vec.data();
             return *this;
         }
 
         VectorSet& operator=(VectorSet&& other) noexcept {
             vec = std::move(other.vec);
             set = std::move(other.set);
-            vec_data = vec.data();
-            other.vec_data = nullptr;
+            vec_data = std::exchange(other.vec_data, nullptr);
             return *this;
         }
 
@@ -76,9 +73,9 @@ namespace cav {
             auto set_elem_iter = set.find(wrapped_elem);
             if (set_elem_iter == set.end()) {
                 vec.emplace_back(std::move(elem));
-                vec_data = vec.data();
+                *vec_data = vec.data();
                 size_t elem_position = vec.size() - 1;
-                set.emplace(vec_data, elem_position);
+                set.emplace(*vec_data, elem_position);
                 return std::make_pair<viter, bool>(vec.begin() + elem_position, true);
             }
 
@@ -115,11 +112,10 @@ namespace cav {
         // Set Operations
         const_viter find(T elem) const {
             T* elem_ptr = std::addressof(elem);
-            auto wrapped_elem = TPtrWrap(elem_ptr, 0);
+            TPtrWrap wrapped_elem(elem_ptr, 0);
 
             const_siter found = set.find(wrapped_elem);
-            if (found != set.cend())
-                return const_viter(found->data());
+            if (found != set.cend()) return const_viter(found->data());
             return vec.cend();
         }
 
@@ -128,8 +124,7 @@ namespace cav {
             TPtrWrap wrapped_elem(elem_ptr, 0);
 
             siter found = set.find(wrapped_elem);
-            if (found != set.end())
-                return viter(found->data());
+            if (found != set.end()) return viter(found->data());
             return vec.end();
         }
 
@@ -138,9 +133,23 @@ namespace cav {
 
         std::vector<T>& get_vector() { return vec; }
 
+        bool is_corrupted() {
+            for (auto elem : set) {
+                if (elem.base_addr != *vec_data) {
+                    fmt::print(stderr, "VectorSet corruped!");
+                    return true;
+                }
+                if (elem.data() < vec.data() || elem.data() >= vec.data() + vec.size()) {
+                    fmt::print(stderr, "VectorSet corruped!");
+                    return true;
+                }
+            }
+            return false;
+        }
+
     private:
         std::vector<T> vec;
-        T* vec_data = nullptr;  // we can't get vec.data() ptr ref, so this is a reproduction that needs to be kept updated
+        T** vec_data = nullptr;  // we can't get vec.data() ptr ref, so this is a reproduction that needs to be kept updated
 
         Set set;
     };
--- one_header_only/SPH.hpp
+++ one_header_only/SPH.hpp
@@ -660,6 +660,7 @@ namespace sph {
 #ifndef CAV_VECTORSET_HPP
 #define CAV_VECTORSET_HPP
 
+#include <cstddef>
 #include <type_traits>
 #include <unordered_set>
 #include <vector>
@@ -673,8 +674,8 @@ namespace cav {
         using const_viter = typename std::vector<T>::const_iterator;
 
         /**
-         * @brief Since the set does not contain T, but *T,
-         * this hacky struct is used to make the *T behave like
+         * @brief Since the set does not contain T, but T*,
+         * this hacky struct is used to make the T* behave like
          * a T would do (for what the set is concerned).
          *
          * Note: it's still a ptr, so it has all the problems of
@@ -687,7 +688,6 @@ namespace cav {
             T* data() const { return base_addr + idx; }
             bool operator==(const TPtrWrap& other) const { return other.base_addr[other.idx] == base_addr[idx]; }
 
-        private:
             T*& base_addr;
             size_t idx;
         };
@@ -697,32 +697,29 @@ namespace cav {
         using const_siter = typename Set::const_iterator;
 
     public:
-        VectorSet() { }
+        VectorSet() : vec_data(new T*) { }
 
         template <typename Iter>
         VectorSet(Iter beg, Iter end) {
             reserve(end - beg);
-            for (auto it = beg; it != end; ++it) {
-                emplace_back(*it);
-            }
+            for (auto it = beg; it != end; ++it) { emplace_back(*it); }
         }
 
-        VectorSet(const VectorSet& other) : vec(other.vec), set(other.set) { vec_data = vec.data(); }
-
-        VectorSet(VectorSet&& other) noexcept : vec(std::move(other.vec)), set(std::move(other.set)) { vec_data = vec.data(); }
+        VectorSet(const VectorSet& other) : vec(other.vec), vec_data(new T*), set(other.set) { *vec_data = vec.data(); }
+        VectorSet(VectorSet&& other) noexcept : vec(std::move(other.vec)), vec_data(std::exchange(other.vec_data, nullptr)), set(std::move(other.set)) { }
+        ~VectorSet() { delete vec_data; }
 
         VectorSet& operator=(const VectorSet& other) {
             vec = other.vec;
             set = other.set;
-            vec_data = vec.data();
+            *vec_data = vec.data();
             return *this;
         }
 
         VectorSet& operator=(VectorSet&& other) noexcept {
             vec = std::move(other.vec);
             set = std::move(other.set);
-            vec_data = vec.data();
-            other.vec_data = nullptr;
+            vec_data = std::exchange(other.vec_data, nullptr);
             return *this;
         }
 
@@ -735,9 +732,9 @@ namespace cav {
             auto set_elem_iter = set.find(wrapped_elem);
             if (set_elem_iter == set.end()) {
                 vec.emplace_back(std::move(elem));
-                vec_data = vec.data();
+                *vec_data = vec.data();
                 size_t elem_position = vec.size() - 1;
-                set.emplace(vec_data, elem_position);
+                set.emplace(*vec_data, elem_position);
                 return std::make_pair<viter, bool>(vec.begin() + elem_position, true);
             }
 
@@ -774,11 +771,10 @@ namespace cav {
         // Set Operations
         const_viter find(T elem) const {
             T* elem_ptr = std::addressof(elem);
-            auto wrapped_elem = TPtrWrap(elem_ptr, 0);
+            TPtrWrap wrapped_elem(elem_ptr, 0);
 
             const_siter found = set.find(wrapped_elem);
-            if (found != set.cend())
-                return const_viter(found->data());
+            if (found != set.cend()) return const_viter(found->data());
             return vec.cend();
         }
 
@@ -787,8 +783,7 @@ namespace cav {
             TPtrWrap wrapped_elem(elem_ptr, 0);
 
             siter found = set.find(wrapped_elem);
-            if (found != set.end())
-                return viter(found->data());
+            if (found != set.end()) return viter(found->data());
             return vec.end();
         }
 
@@ -797,9 +792,23 @@ namespace cav {
 
         std::vector<T>& get_vector() { return vec; }
 
+        bool is_corrupted() {
+            for (auto elem : set) {
+                if (elem.base_addr != *vec_data) {
+                    fmt::print(stderr, "VectorSet corruped!");
+                    return true;
+                }
+                if (elem.data() < vec.data() || elem.data() >= vec.data() + vec.size()) {
+                    fmt::print(stderr, "VectorSet corruped!");
+                    return true;
+                }
+            }
+            return false;
+        }
+
     private:
         std::vector<T> vec;
-        T* vec_data = nullptr;  // we can't get vec.data() ptr ref, so this is a reproduction that needs to be kept updated
+        T** vec_data = nullptr;  // we can't get vec.data() ptr ref, so this is a reproduction that needs to be kept updated
 
         Set set;
     };
